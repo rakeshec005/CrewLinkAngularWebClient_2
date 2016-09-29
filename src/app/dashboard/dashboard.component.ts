93f5658080c65effffe92b90aae39d6e2c129db2
rakesh <rak1994kumar@gmail.com>
2016-09-29 01:14:36 -0700

    added router in dasdas

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 'use strict';
 import { Component} from '@angular/core';
 
+
 @Component({
   selector: 'app-blank',
   templateUrl:"dashboard.component.html",
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,12 +6,13 @@ import { userComponent } from '../app/user/user.component';
 import { dashboardComponent } from '../app/dashboard/dashboard.component';
 import { loginComponent } from '../app/login/login.component';
 
+
 export const APP_ROUTER_PROVIDERS: Routes = [
-  { path: '', component: dashboardComponent },
+  { path: 'login', component: loginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: aboutComponent },
   { path: 'userRole', component: userRoleComponent },
   { path: 'user', component: userComponent },
-  { path: 'login', component: loginComponent }
+  { path: '', component: dashboardComponent }
 ];
 
